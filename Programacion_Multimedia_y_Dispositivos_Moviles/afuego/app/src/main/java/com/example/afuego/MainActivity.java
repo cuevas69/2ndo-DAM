65b197e124f962ca9287ce954750c0365b725d6b
cuevas69 <andrescuevas507@gmail.com>
2023-10-18 10:54:06 +0200

    18/10/23

--- Programacion_Multimedia_y_Dispositivos_Moviles/afuego/app/src/main/java/com/example/afuego/MainActivity.java
+++ Programacion_Multimedia_y_Dispositivos_Moviles/afuego/app/src/main/java/com/example/afuego/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,8 +15,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button bPerfil = findViewById(R.id.bPerfil);
-        bPerfil.setOnClickListener(new View.OnClickListener(){
+        ImageView perfil = findViewById(R.id.perfil);
+        perfil.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 Intent intent = new Intent(v.getContext(),PagePerfil.class);
--- Programacion_Multimedia_y_Dispositivos_Moviles/afuego/app/src/main/java/com/example/afuego/PageConfig.java
+++ Programacion_Multimedia_y_Dispositivos_Moviles/afuego/app/src/main/java/com/example/afuego/PageConfig.java
@@ -9,27 +9,18 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
-public class PagePerfil extends AppCompatActivity {
+public class PageConfig extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.page_perfil);
+        setContentView(R.layout.page_config);
 
         ImageView inicio = findViewById(R.id.inicio);
         inicio.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 Intent intent = new Intent(v.getContext(),MainActivity.class);
-                startActivityForResult(intent, 0);
-            }
-        });
-
-        ImageView ajustes = findViewById(R.id.ajustes);
-        ajustes.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Intent intent = new Intent(v.getContext(),PageConfig.class);
                 startActivityForResult(intent, 0);
             }
         });
